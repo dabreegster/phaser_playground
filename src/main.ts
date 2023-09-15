990bc97e169a3cc0a22d784bff0e337ca4b43d8e
Dustin Carlino <dabreegster@gmail.com>
2023-09-15 19:46:25 +0100

    Camera controls

--- src/main.ts
+++ src/main.ts
@@ -7,19 +7,40 @@ class MainScene extends Scene {
 
   async create() {
     let buildings = await getMapspaceBuildings();
-
     for (let polygon of buildings) {
       this.add.polygon(0, 0, polygon, "0x6666ff");
     }
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.zoom_in = this.input.keyboard.addKey("Q");
+    this.zoom_out = this.input.keyboard.addKey("W");
   }
 
-  /*update(_time: number, delta: number) {
-    if (!this.textbox) {
+  update(_time: number, _delta: number) {
+    if (!this.cursors || !this.zoom_in || !this.zoom_out) {
       return;
     }
 
-    this.textbox.rotation += 0.005 * delta;
-  }*/
+    // https://github.com/photonstorm/phaser3-examples/blob/master/public/src/camera/follow%20zoom.js might be better
+    if (this.cursors.left.isDown) {
+      this.cameras.main.scrollX -= 5;
+    } else if (this.cursors.right.isDown) {
+      this.cameras.main.scrollX += 5;
+    }
+
+    if (this.cursors.up.isDown) {
+      this.cameras.main.scrollY -= 5;
+    } else if (this.cursors.down.isDown) {
+      this.cameras.main.scrollY += 5;
+    }
+
+    if (this.zoom_in.isDown) {
+      this.cameras.main.zoom += 0.1;
+    }
+    if (this.zoom_out.isDown) {
+      this.cameras.main.zoom -= 0.1;
+    }
+  }
 }
 
 const config: Phaser.Types.Core.GameConfig = {
