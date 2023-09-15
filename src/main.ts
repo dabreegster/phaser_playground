8e93f2c9970cfbae1bf0c3c3e71dceb0d5ce4e5f
Dustin Carlino <dabreegster@gmail.com>
2023-09-15 19:05:02 +0100

    Setup phaser

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,37 @@
 import "./style.css";
+import { Scene, Game, GameObjects } from "phaser";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `hey`;
+class MainScene extends Scene {
+  private textbox: GameObjects.Text | undefined;
+
+  create() {
+    this.textbox = this.add.text(
+      window.innerWidth / 2,
+      window.innerHeight / 2,
+      "Hello",
+      {
+        color: "red",
+        fontSize: "50px",
+      },
+    );
+
+    this.textbox.setOrigin(0.5, 0.5);
+  }
+
+  update(_time: number, delta: number) {
+    if (!this.textbox) {
+      return;
+    }
+
+    this.textbox.rotation += 0.005 * delta;
+  }
+}
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  width: window.innerWidth,
+  height: window.innerHeight,
+  scene: MainScene,
+};
+
+new Game(config);
